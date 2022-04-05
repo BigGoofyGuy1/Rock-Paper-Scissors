5bc7d7069890420718ba3f8df1b987d1c2729bd9
BigGoofyGuy <collin.weatherford@gmail.com>
2022-04-05 16:26:18 -0500

    Completed Game Function

--- script.js
+++ script.js
@@ -35,15 +35,14 @@ function playerInput() {
         playerChoice = "Paper"; 
     } else if (userInput == '3') {
         playerChoice = "Scissors"; 
-    } else {
-        console.log("Invalid selection. Please close and try again.")
-    }
+    } 
 }
 
-
+let winner = '';
 
 function playRound() {
 
+    console.clear();
     computerPlay();
     playerInput();
 
@@ -52,34 +51,45 @@ function playRound() {
 
     compChoice = compChoice.toLowerCase();
     playerChoice = playerChoice.toLowerCase();
+    
 
     if (playerChoice == compChoice) {
         console.log("It's a tie! Please play again!");
+        winner = 'tie'
     }
 
     if (playerChoice == 'rock') {
         if (compChoice == 'paper') {
-            console.log("Rock Covers Paper! You win!");
+            console.log("Paper Covers Rock! You lose!");
+            winner = 'computer'
         } else if (compChoice == 'scissors') {
             console.log("Rock breaks Scissors! You win!");
+            winner = 'player'
         }
     }
 
     if (playerChoice == 'paper') {
         if (compChoice == 'rock') {
-            console.log("Rock Covers Paper! You lose!")
+            console.log("Paper Covers Rock! You win!")
+            winner = 'player'
         } else if (compChoice == 'scissors') {
             console.log("Scissors cut Paper! You lose!")
+            winner = 'computer'
         }
     }
 
     if (playerChoice == 'scissors') {
         if (compChoice == 'rock') {
-            consol.log("Rock breaks Scissors! You lose!")
+            console.log("Rock breaks Scissors! You lose!")
+            winner = 'computer'
         } else if (compChoice == 'paper') {
             console.log("Scissors cut Paper! You win!")
+            winner = 'player'
         }
     }
+
+    return winner;
+    
 
 }
 
@@ -88,8 +98,23 @@ function game() {
 
     console.log("Rounds to play: " + roundsToPlay)
 
+    let playerWins = 0
+    let compWins = 0
+    let tieCount = 0
     for (let i = 0; i < roundsToPlay; i++) {
         playRound();
+
+        if (winner == 'player') {
+            playerWins = playerWins +1
+        } else if ( winner == 'computer') {
+            compWins = compWins +1
+        } else {
+            tieCount = tieCount +1
+        }
+        console.log("Player Wins: " + playerWins)
+        console.log("Computer Wins: " + compWins)
+        console.log("Ties : " + tieCount)
+        
     }
 }
 
